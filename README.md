788d0a91324fb891a1fe315cf4b8bba117e24f51
John Halloran <john.p.halloran@marquette.edu>
2016-07-27 20:36:15 -0400

    Basic constructor for rubik's cube

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Rubik's Solution Application
 
 Goal: To design a working application that will solve a rubik's cube in an efficient manner both time and memory
+
+Credit is due where credit is due: 
+
+Howard A. Pelle, "Representing Rubik's Cube in APL". http://delivery.acm.org/10.1145/810000/801107/p255-peelle.pdf?ip=134.48.233.72&id=801107&acc=ACTIVE%20SERVICE&key=5E5F143D919E1EED%2E7A460435F505E86F%2E4D4702B0C3E38B35%2E4D4702B0C3E38B35&CFID=818886960&CFTOKEN=19453591&__acm__=1469665738_589f8d892e6d8daeb5201b1a9df33376
+
+
--- src/cube/RubiksCube.java
+++ src/cube/RubiksCube.java
@@ -1,36 +1,50 @@
 package cube;
 
 public class RubiksCube {
-	private char face1[][];
-	private char face2[][];
-	private char face3[][];
-	private char face4[][];
-	private char face5[][];
-	private char face6[][];
-	
-	
-	/** 
-	 * Randomly generates a rubiks cube
-	 */
-	public RubiksCube(){
-		
-	}
-	
-	public RubiksCube(char face1[][], char face2[][], char face3[][] ,char face4[][] ,char face5[][], char face6[][])
-	{
-	    copyArray(this.face1, face1);
-	    copyArray(this.face2, face2);
-	    copyArray(this.face3, face3);
-	    copyArray(this.face4, face4);
-	    copyArray(this.face5, face5);
-	    copyArray(this.face6, face6);
-	}
+    private char face1[][];
+    private char face2[][];
+    private char face3[][];
+    private char face4[][];
+    private char face5[][];
+    private char face6[][];
 
-    private void copyArray(
-            char[][] face62,
-            char[][] face63) {
-        // TODO Auto-generated method stub
-        
+    /**
+     * Randomly generates a rubiks cube
+     */
+    public RubiksCube() {
+
     }
 
+    public RubiksCube(char face1[][], char face2[][], char face3[][],
+            char face4[][], char face5[][], char face6[][]) {
+        this.face1 = new char[3][3];
+        this.face2 = new char[3][3];
+        this.face3 = new char[3][3];
+        this.face4 = new char[3][3];
+        this.face5 = new char[3][3];
+        this.face6 = new char[3][3];
+
+        copyArray(this.face1, face1);
+        copyArray(this.face2, face2);
+        copyArray(this.face3, face3);
+        copyArray(this.face4, face4);
+        copyArray(this.face5, face5);
+        copyArray(this.face6, face6);
+    }
+
+    /**
+     * For copying arrays.
+     * 
+     * @param thisObject
+     *            face of the cube
+     * @param newFace
+     *            new face of the cube
+     */
+    private void copyArray(char[][] thisObject, char[][] newFace) {
+        for (int x = 0; x < thisObject.length; x++) {
+            for (int y = 0; y < thisObject[x].length; y++) {
+                thisObject[x][y] = newFace[x][y];
+            }
+        }
+    }
 }
